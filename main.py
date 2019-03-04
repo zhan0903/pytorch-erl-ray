8531a8d2ff69ee9b57340e24eeee38e4afe5a034
zhan0903 <z.han0903@qq.com>
2019-03-05 09:45:24 +1100

    debug

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     np.random.seed(parameters.seed)
     random.seed(parameters.seed)
     evolver = utils_ne.SSNE(parameters)
-    print("random,",random.randint())
+    print("random,",random.randint(10))
 
     pops_new = []
     for _ in range(parameters.pop_size):
