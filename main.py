b2af1469c462415f3f50690a7199b06a6fd93624
zhan0903 <z.han0903@qq.com>
2019-03-15 10:00:35 +1100

    debug

--- main.py
+++ main.py
@@ -263,6 +263,9 @@ def apply_grads(net,grads_actor,grads_critic):
     print(next(net.actor.parameters()).device)
     print(next(net.critic.parameters()).device)
 
+    print(grads_critic.get_device())
+    print(grads_actor.get_device())
+
     net.actor_optimizer.zero_grad()
     grads_sum_actor = copy.deepcopy(grads_actor[-1])
     for grad in grads_actor[:-1]:
