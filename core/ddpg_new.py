90e264f7d72ee10ce2050288d49a1dcad91900eb
zhan0903 <z.han0903@qq.com>
2019-03-17 20:38:44 +1100

    debug

--- core/ddpg_new.py
+++ core/ddpg_new.py
@@ -94,7 +94,7 @@ class DDPG(object):
         self.grads_actor = []
         self.grads_critic = []
 
-        for it in range(1):
+        for it in range(2):
 
             # Sample replay buffer
             x, y, u, r, d = replay_buffer.sample(batch_size)
