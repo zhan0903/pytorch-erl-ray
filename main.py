635faa58312ddbb48f2b7701c93008943259af4e
zhan0903 <z.han0903@qq.com>
2019-03-19 21:46:47 +1100

    debug

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
         # else:
         #     actor_weight = None
         critic_id = ray.put(agent.critic.state_dict())
-        train_id = [worker.train.remote(None, critic_id) for worker, actor in zip(workers[:-1], agent.actors)] # actor.state_dict()
+        train_id = [worker.train.remote(actor.state_dict(), critic_id) for worker, actor in zip(workers[:-1], agent.actors)] # actor.state_dict()
         results = ray.get(train_id)
         total_timesteps, grads_critic, all_fitness, all_id = process_results(results)
         agent.apply_grads(grads_critic)
@@ -290,7 +290,7 @@ if __name__ == "__main__":
         # debug = False
         print("after apply_grads self.policy.critic,", agent.critic.state_dict()["l3.bias"])
         # if episode // 3 == 0:
-        #     elite_index = evolver.epoch(actors, all_fitness)
+        elite_index = evolver.epoch(agent.actors, all_fitness)
         #     print("elite_index,",elite_index)
         # else:
 
