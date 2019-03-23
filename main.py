69571ca47270a26049ff08238d5200a918222da2
zhan0903 <z.han0903@qq.com>
2019-03-23 12:36:43 +1100

    test 100 evovle for 4 pop

--- main.py
+++ main.py
@@ -311,19 +311,19 @@ if __name__ == "__main__":
 
         logger_main.debug("episode:{}".format(episode))
 
-        if evolve: # evolve # True
+        if True: # evolve # True
             logger_main.info("before evolve actor weight 0:{}".format(agent.actors[0].state_dict()["w_l1.weight"][1][:5]))
             logger_main.info("before evolve actor weight 1:{}".format(agent.actors[1].state_dict()["w_l1.weight"][1][:5]))
             logger_main.info("before evolve actor weight 2:{}".format(agent.actors[2].state_dict()["w_l1.weight"][1][:5]))
             logger_main.info("before evolve actor weight 3:{}".format(agent.actors[3].state_dict()["w_l1.weight"][1][:5]))
 
-        if evolve: # evolve
+        if True: # evolve
             evolver.epoch(agent.actors, all_fitness)
             actors = [actor.state_dict() for actor in agent.actors]
         else:
             actors = [None for _ in range(args.pop_size)]
 
-        if evolve: # evolve
+        if True: # evolve
             logger_main.info("after actor weight 0:{}".format(agent.actors[0].state_dict()["w_l1.weight"][1][:5]))
             logger_main.info("after actor weight 1,{}".format(agent.actors[1].state_dict()["w_l1.weight"][1][:5]))
             logger_main.info("after actor weight 2,{}".format(agent.actors[2].state_dict()["w_l1.weight"][1][:5]))
