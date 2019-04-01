ece72c80302da8ff55137105446abc90a31e9e72
zhan0903 <z.han0903@qq.com>
2019-04-01 21:32:40 +1100

    change up_down limit for walker2d

--- main.py
+++ main.py
@@ -247,8 +247,8 @@ if __name__ == "__main__":
     parser.add_argument("--node_name", default="qcis5")
     parser.add_argument("--version_name")
 
-    up_limit = 3e5
-    down_limit = 2e5
+    up_limit = 1.5e5
+    down_limit = 1e5
 
     args = parser.parse_args()
 
