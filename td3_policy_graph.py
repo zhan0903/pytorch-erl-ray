36cf0b2da1912b3f72020e261e1fb4a5e2c78e81
zhan0903 <z.han0903@qq.com>
2019-04-26 17:10:08 +1000

    debug

--- td3_policy_graph.py
+++ td3_policy_graph.py
@@ -8,7 +8,7 @@ from utils import to_numpy
 from copy import deepcopy
 import pysnooper
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Actor(nn.Module):
