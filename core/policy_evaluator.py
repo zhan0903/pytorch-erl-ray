b51eedc2540bce3a30a55ba99d88c8424db59ac7
zhan0903 <z.han0903@qq.com>
2019-04-27 11:18:23 +1000

    debug

--- core/policy_evaluator.py
+++ core/policy_evaluator.py
@@ -18,7 +18,7 @@ from ray.rllib.env.vector_env import VectorEnv
 from ray.rllib.evaluation.interface import EvaluatorInterface
 from ray.rllib.evaluation.sample_batch import MultiAgentBatch, \
     DEFAULT_POLICY_ID
-from ray.rllib.evaluation.sampler import AsyncSampler, SyncSampler
+from core.sampler import AsyncSampler, SyncSampler
 from ray.rllib.evaluation.policy_graph import PolicyGraph
 from ray.rllib.evaluation.tf_policy_graph import TFPolicyGraph
 from ray.rllib.offline import NoopOutput, IOContext, OutputWriter, InputReader
