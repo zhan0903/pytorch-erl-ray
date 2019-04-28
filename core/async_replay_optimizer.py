7feb4b5cf53e7711325a97309ad6a5de76376606
zhan0903 <z.han0903@qq.com>
2019-04-28 20:22:17 +1000

    debug

--- core/async_replay_optimizer.py
+++ core/async_replay_optimizer.py
@@ -365,7 +365,7 @@ class BatchReplayActor(object):
     def get_host(self):
         return os.uname()[1]
 
-    @pysnooper.snoop()
+    # @pysnooper.snoop()
     def add_batch(self, batch):
         # Handle everything as if multiagent
         if isinstance(batch, SampleBatch):
